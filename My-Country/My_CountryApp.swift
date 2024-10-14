54e0ea1fb379a804b15e68fe78ba0574c9ea1285
Abdirahman <mustafeabdisalam@gmail.com>
2024-10-14 19:57:35 +0300

    Initial Commit

---
+++ My-Country/My_CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CountryApp.swift
+//  My-Country
+//
+//  Created by Abdirahman Standard on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct My_CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
